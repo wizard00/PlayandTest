8711e2a4364853d14065dac522d7708b7921a057
Yufei Wang <Yufei-Mac@Yufeis-MacBook-Pro.local>
2016-01-09 15:51:53 -0800

    finish basic logic

--- piglatin.cpp
+++ piglatin.cpp
@@ -1,5 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namesapce std;
+//letter before vowel are placed at the end, + ay
+//if starting with vowel, +yay
+
 int main(int argc, char const *argv[])
 {
 	/* code */
 	return 0;
+}
+
+string pigLatin(string s) {
+	int n = s.length();
+	string res;
+	if(isvowel(s[0])){
+		res = s+"y";
+	}
+	else {
+		for (int i = 1; i < n; ++i)
+		{
+			if(isvowel(s[i])){
+				res = s.substr(i,n-i)+s.substr(0,i);
+				break;
+			}
+		}
+	}	
+
+	res+="ay";
+	return res;
+
 }
